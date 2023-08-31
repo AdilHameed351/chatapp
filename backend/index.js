c83e621095d10c0fbe383215fc7166ca5fe3d809
adilhameed351 <adilhameed674@gmail.com>
2023-08-31 20:31:18 +0500

    chatapp

---
+++ backend/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+
+const app = express();
+app.use(express.json());
+app.use(cors({origin: true}));
+
+app.post("/authenticate", async (req, res) => {
+    const { username } = req.body;
+
+    try {
+        const r = await axios.put("https://api.chatengine.io/users/",{username: username, secret: username, first_name: username},{headers:{"private-key":"5e7a14f5-deb3-4037-8745-a8e0677d4dae"}});
+        return res.status(r.status).json(r.data);
+    } catch (e) {
+        return res.status(e.response.status).json(e.response.data);
+    }
+});
+
+app.listen(3001);
