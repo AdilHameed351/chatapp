b1334120ef5f3667b74258e979408b08c0a9d3fd
AdilHameed351 <59979658+AdilHameed351@users.noreply.github.com>
2023-08-31 21:07:33 +0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,13 @@ const axios = require("axios");
 
 const app = express();
 app.use(express.json());
-app.use(cors({origin: true}));
+app.use(cors(
+    {
+        origin: ["https://chatapp-frontend-three.vercel.app/"],
+        methods: ["POST", "GET"],
+        credentials: true
+    }
+));
 
 app.post("/authenticate", async (req, res) => {
     const { username } = req.body;
