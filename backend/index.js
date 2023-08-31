217d32fa1bea139fe1f672e55e7042bdcfc5259f
AdilHameed351 <59979658+AdilHameed351@users.noreply.github.com>
2023-08-31 21:08:26 +0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json());
 app.use(cors(
     {
-        origin: ["https://chatapp-frontend-three.vercel.app/"],
+        origin: ["https://chatapp-frontend-three.vercel.app"],
         methods: ["POST", "GET"],
         credentials: true
     }
